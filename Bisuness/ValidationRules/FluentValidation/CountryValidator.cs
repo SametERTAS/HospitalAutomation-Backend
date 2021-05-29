6754c6d669dcd67e2fbb3ef61f2dcdf55ab153cf
SametERTAS <samet.ertas@outlook.com>
2021-05-29 13:14:55 +0300

    Fluent Validation was added.

---
+++ Bisuness/ValidationRules/FluentValidation/CountryValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CountryValidator : AbstractValidator<Country>
+    {
+        public CountryValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+            RuleFor(c => c.Name).Length(3, 30);
+           
+        }
+    }
+}
