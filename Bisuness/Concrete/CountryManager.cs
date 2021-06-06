309767d42767699fc750a03fbb00dd50457b4547
SametERTAS <samet.ertas@outlook.com>
2021-06-06 17:04:02 +0300

    FluentValidationTool was updated

--- Bisuness/Concrete/CountryManager.cs
+++ Bisuness/Concrete/CountryManager.cs
@@ -1,5 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Result.Abstract;
 using Core.Utilities.Result.Concrete;
 using DataAccess.Abstract;
@@ -19,6 +21,7 @@ namespace Business.Concrete
         {
             _countryDal = countryDal;
         }
+        [ValidationAspect(typeof(CountryValidator),Priority =1)]
         public IResult Add(Country country)
         {
             _countryDal.Add(country);
--- Core/CrossCuttingConcerns/Validation/FluentValidation/FluentValidationTool.cs
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/FluentValidationTool.cs
@@ -4,19 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.Entities.Abstract;
 
 namespace Core.CrossCuttingConcerns.Validation.FluentValidation
 {
     public static class FluentValidationTool
     {
-
         public static void Validate(IValidator validator, object entity)
         {
-            var result = validator.Validate((IValidationContext)entity);
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
             if (!result.IsValid)
             {
                 throw new ValidationException(result.Errors);
             }
         }
+
     }
 }
